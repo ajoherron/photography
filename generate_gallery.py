2f33de25bc3dc04910d3d4e02e31dbd58432f7f9
Alex Herron <ajoherron@gmail.com>
2026-06-29 10:31:56 -0400

    Add roll 31

--- generate_gallery.py
+++ generate_gallery.py
@@ -3,9 +3,9 @@ import os
 ##############
 ### Update ###
 ##############
-GALLERY = "gallery28"  # E.g., gallery15
-DATE = "2/11/2025"  # E.g., 4/27/2025
-FILM = "Lomography Metropolis 400 (35mm)"  # E.g., Kodak UltraMax 400 (35mm)
+GALLERY = "gallery31"  # E.g., gallery15
+DATE = "6/12/2026"  # E.g., 4/27/2025
+FILM = "Kodak Portra 160 (35mm)"  # E.g., Kodak UltraMax 400 (35mm)
 CAMERA = "Canon AE-1 Program"  # E.g., Canon AE-1 Program, Olympus Stylus Epic Zoom 170, Nikon Photomic FTn
 
 
